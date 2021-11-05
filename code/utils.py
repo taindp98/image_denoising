f815f9d57054b509325b03dc58ea98392aef0d5f
Tai NGUYEN <taindp98@gmail.com>
2021-11-05 19:31:48 +0100

    done pca

---
+++ code/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pywt
+
+def add_noisy(img, var=None):
+    """
+    create a noise array with the same size as the input image
+    add noise array into the input image
+    """
+    row,col= img.shape
+    mean = 0
+    if not var:
+        var = 25
+    sigma = var**0.5
+    gauss = np.random.normal(mean,sigma,(row,col))
+    gauss = gauss.reshape(row,col)
+    noisy = img + gauss
+    return noisy, gauss
+
+def transform_wavelet(spatial):
+    coeffs2 = pywt.dwt2(spatial, 'db2')
+    return coeffs2
